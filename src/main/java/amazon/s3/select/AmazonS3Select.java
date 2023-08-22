fc38a864549813128f5b8902fe91a935885c6890
Srikar Reddy Kalikiri <kalikirisrikarreddy@gmail.com>
2023-08-22 19:49:22 +0530

    added sample files of 100K records to work with amazon s3 select

--- src/main/java/amazon/s3/select/AmazonS3Select.java
+++ src/main/java/amazon/s3/select/AmazonS3Select.java
@@ -17,9 +17,9 @@ import com.amazonaws.services.s3.model.CSVOutput;
 import com.amazonaws.services.s3.model.ExpressionType;
 import com.amazonaws.services.s3.model.InputSerialization;
 import com.amazonaws.services.s3.model.JSONInput;
-import com.amazonaws.services.s3.model.JSONOutput;
 import com.amazonaws.services.s3.model.JSONType;
 import com.amazonaws.services.s3.model.OutputSerialization;
+import com.amazonaws.services.s3.model.ParquetInput;
 import com.amazonaws.services.s3.model.SelectObjectContentEvent;
 import com.amazonaws.services.s3.model.SelectObjectContentEvent.RecordsEvent;
 import com.amazonaws.services.s3.model.SelectObjectContentEventStream;
@@ -27,21 +27,18 @@ import com.amazonaws.services.s3.model.SelectObjectContentRequest;
 import com.amazonaws.services.s3.model.SelectObjectContentResult;
 import com.fasterxml.jackson.core.exc.StreamWriteException;
 import com.fasterxml.jackson.databind.DatabindException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvParser;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.github.javafaker.Faker;
 
 public class AmazonS3Select {
 
-	private static final int N = 100 * 1000;
+	private static final int N = 5;
 	private static final String BUCKET_NAME = "amazon-s3-select-"
 			+ LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
 	private static final String EMPLOYEES_JSON_FILE_NAME = "employees.json";
 	private static final String EMPLOYEES_CSV_FILE_NAME = "employees.csv";
+	private static final String EMPLOYEES_PARQUET_FILE_NAME = "employees.parquet";
 	private static final String NEW_LINE = "\n";
+	private static final String COMMA = "\n";
 
 	public static void main(String[] args) throws IOException {
 
@@ -50,19 +47,27 @@ public class AmazonS3Select {
 		createBucketIfNotExists(amazonS3);
 
 		Employees employees = generateEmployeeData();
-		
-		long startTimeInMillis = System.currentTimeMillis();
-		exportEmployeesInJsonFormat(employees, amazonS3);
-		long endTimeInMillis = System.currentTimeMillis();
-		System.out.println("Took "+(endTimeInMillis - startTimeInMillis) + " ms to get 5 random records matching a certain age group from 100K records in json format");
-		
-		startTimeInMillis = System.currentTimeMillis();
-		exportEmployeesInCSVFormat(employees, amazonS3);
-		endTimeInMillis = System.currentTimeMillis();
-		System.out.println("Took "+(endTimeInMillis - startTimeInMillis) + " ms to get 5 random records matching a certain age group from 100K records in csv format");
 
-		getAny5EmployeesWhoseAgeIsGreaterThan50FromEmployeesCsvObject(amazonS3);
+		exportEmployeesInJsonFormat(employees, amazonS3);
+		exportEmployeesInCSVFormat(employees, amazonS3);
+		exportEmployeesInParquetFormat(employees, amazonS3);
+
+		long t1 = System.currentTimeMillis();
 		getAny5EmployeesWhoseAgeIsGreaterThan50FromEmployeesJsonObject(amazonS3);
+		long t2 = System.currentTimeMillis();
+		System.out.println("Time to read json : " + (t2 - t1) + " ms");
+		System.out.println("------------------------------------------------------------");
+
+		t1 = System.currentTimeMillis();
+		getAny5EmployeesWhoseAgeIsGreaterThan50FromEmployeesCsvObject(amazonS3);
+		t2 = System.currentTimeMillis();
+		System.out.println("Time to read csv : " + (t2 - t1) + " ms");
+		System.out.println("------------------------------------------------------------");
+
+		t1 = System.currentTimeMillis();
+		getAny5EmployeesWhoseAgeIsGreaterThan50FromEmployeesParquetObject(amazonS3);
+		t2 = System.currentTimeMillis();
+		System.out.println("Time to read parquet : " + (t2 - t1) + " ms");
 
 	}
 
@@ -75,18 +80,17 @@ public class AmazonS3Select {
 		selectObjectContentRequest.setExpression("select s.* from S3Object[*].employees[*] s where s.age > 50 limit 5");
 		selectObjectContentRequest
 				.setInputSerialization(new InputSerialization().withJson(new JSONInput().withType(JSONType.DOCUMENT)));
-		selectObjectContentRequest.setOutputSerialization(
-				new OutputSerialization().withJson(new JSONOutput().withRecordDelimiter(NEW_LINE)));
+		selectObjectContentRequest.setOutputSerialization(new OutputSerialization().withCsv(new CSVOutput()));
 		SelectObjectContentResult selectObjectContentResult = amazonS3.selectObjectContent(selectObjectContentRequest);
 		SelectObjectContentEventStream selectObjectContentEventStream = selectObjectContentResult.getPayload();
 		Iterator<SelectObjectContentEvent> eventsIterator = selectObjectContentEventStream.getEventsIterator();
 		while (eventsIterator.hasNext()) {
 			SelectObjectContentEvent selectObjectContentEvent = eventsIterator.next();
-			if(selectObjectContentEvent instanceof RecordsEvent) {
+			if (selectObjectContentEvent instanceof RecordsEvent) {
 				RecordsEvent recordsEvent = (RecordsEvent) selectObjectContentEvent;
 				System.out.println(new String(recordsEvent.getPayload().array()));
 			}
-		} 
+		}
 		selectObjectContentEventStream.close();
 		selectObjectContentResult.close();
 	}
@@ -105,7 +109,30 @@ public class AmazonS3Select {
 		Iterator<SelectObjectContentEvent> eventsIterator = selectObjectContentEventStream.getEventsIterator();
 		while (eventsIterator.hasNext()) {
 			SelectObjectContentEvent selectObjectContentEvent = eventsIterator.next();
-			if(selectObjectContentEvent instanceof RecordsEvent) {
+			if (selectObjectContentEvent instanceof RecordsEvent) {
+				RecordsEvent recordsEvent = (RecordsEvent) selectObjectContentEvent;
+				System.out.println(new String(recordsEvent.getPayload().array()));
+			}
+		}
+		selectObjectContentEventStream.close();
+		selectObjectContentResult.close();
+	}
+
+	private static void getAny5EmployeesWhoseAgeIsGreaterThan50FromEmployeesParquetObject(AmazonS3 amazonS3)
+			throws IOException {
+		SelectObjectContentRequest selectObjectContentRequest = new SelectObjectContentRequest();
+		selectObjectContentRequest.setBucketName(BUCKET_NAME);
+		selectObjectContentRequest.setKey(EMPLOYEES_PARQUET_FILE_NAME);
+		selectObjectContentRequest.setExpressionType(ExpressionType.SQL);
+		selectObjectContentRequest.setExpression("select * from S3Object where column2 > 50 limit 5");
+		selectObjectContentRequest.setInputSerialization(new InputSerialization().withParquet(new ParquetInput()));
+		selectObjectContentRequest.setOutputSerialization(new OutputSerialization().withCsv(new CSVOutput()));
+		SelectObjectContentResult selectObjectContentResult = amazonS3.selectObjectContent(selectObjectContentRequest);
+		SelectObjectContentEventStream selectObjectContentEventStream = selectObjectContentResult.getPayload();
+		Iterator<SelectObjectContentEvent> eventsIterator = selectObjectContentEventStream.getEventsIterator();
+		while (eventsIterator.hasNext()) {
+			SelectObjectContentEvent selectObjectContentEvent = eventsIterator.next();
+			if (selectObjectContentEvent instanceof RecordsEvent) {
 				RecordsEvent recordsEvent = (RecordsEvent) selectObjectContentEvent;
 				System.out.println(new String(recordsEvent.getPayload().array()));
 			}
@@ -123,7 +150,7 @@ public class AmazonS3Select {
 	private static AmazonS3 getAmazonS3Client() {
 		return AmazonS3ClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(
-						new BasicAWSCredentials("AKIA5DV4LDPDAFFXZZUN", "Fh+rFDSjcznRHljt3KevXGq9QBpufYL8O7P3zswh")))
+						new BasicAWSCredentials("AKIA5DV4LDPDPQXDR6JO", "pQV8JRNWu62nT17qTAok8x5c97E4p53AfWDZvwNK")))
 				.withRegion("us-east-1").build();
 	}
 
@@ -140,22 +167,20 @@ public class AmazonS3Select {
 
 	private static void exportEmployeesInJsonFormat(Employees employees, AmazonS3 amazonS3)
 			throws IOException, StreamWriteException, DatabindException {
-		ObjectMapper jsonMapper = new ObjectMapper();
-		ObjectWriter jsonWriter = jsonMapper.writerFor(Employees.class).withDefaultPrettyPrinter();
 		File employeesJsonFile = new File(EMPLOYEES_JSON_FILE_NAME);
-		jsonWriter.writeValue(employeesJsonFile, employees);
 		amazonS3.putObject(BUCKET_NAME, EMPLOYEES_JSON_FILE_NAME, employeesJsonFile);
 	}
 
 	private static void exportEmployeesInCSVFormat(Employees employees, AmazonS3 amazonS3)
 			throws IOException, StreamWriteException, DatabindException {
-		CsvMapper csvMapper = new CsvMapper();
-		csvMapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
-		CsvSchema employeesCsvSchema = CsvSchema.builder().addColumn("id").addColumn("name").addColumn("age").build();
-		ObjectWriter csvWriter = csvMapper.writerFor(List.class).with(employeesCsvSchema).withDefaultPrettyPrinter();
 		File employeesCsvFile = new File(EMPLOYEES_CSV_FILE_NAME);
-		csvWriter.writeValue(employeesCsvFile, employees.employees());
 		amazonS3.putObject(BUCKET_NAME, EMPLOYEES_CSV_FILE_NAME, employeesCsvFile);
+	}
+
+	private static void exportEmployeesInParquetFormat(Employees employees, AmazonS3 amazonS3)
+			throws StreamWriteException, DatabindException, IOException {
+		File employeesParquetFile = new File(EMPLOYEES_PARQUET_FILE_NAME);
+		amazonS3.putObject(BUCKET_NAME, EMPLOYEES_PARQUET_FILE_NAME, employeesParquetFile);
 	}
 
 }
